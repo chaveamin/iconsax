d38b9ac5d52c6139c7ce5827430498dbace53236
Amin <aminchave@gmail.com>
2026-04-23 21:00:02 +0330

    output:export

--- src/app/api/search-index/route.ts
+++
@@ -1,8 +0,0 @@
-import { getSearchIndex } from "@/src/lib/searchIndex";
-
-export async function GET() {
-  const index = getSearchIndex();
-  // Convert Map to plain object
-  const obj = Object.fromEntries(index.entries());
-  return Response.json(obj);
-}
